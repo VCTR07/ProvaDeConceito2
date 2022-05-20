dbf7a9ca742ee6c5132fd9119b8b53778e27540a
Victor Emanuel <victor_emanuell@yahoo.com.br>
2022-05-19 22:12:38 -0300

    exercicio finalizado

--- app/views.py
+++ app/views.py
@@ -8,13 +8,8 @@ import datetime
 def horas():
     return str(datetime.datetime.now())
 
-@app.route('/soma')
-def template():
-    return render_template('soma.html')
-
-@app.route('/soma', methods=['POST'])
-def my_form_post():
-    n1 = request.form['numero1']
-    n2 = request.form['numero2']
+@app.route('/soma/<n1>/<n2>')
+def my_form_post(n1,n2):
+    
 
     return f"a soma dos dois numero deu {float(n1) + float(n2)}"
