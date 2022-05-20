15e2dd130c1bcf8adc8649d5b3e815b9c27cbedd
Victor Emanuel <victor_emanuell@yahoo.com.br>
2022-05-19 21:49:58 -0300

    hora soma

--- app/views.py
+++ app/views.py
@@ -1,10 +1,20 @@
-from flask import render_template
-from app import app
+from flask import render_template, request, Flask
 
-@app.route('/')
-def home():
-    return "<b>There has been a change</b>"
+app = Flask(__name__)
 
-@app.route('/template')
+import datetime
+
+@app.route('/hora')
+def horas():
+    return str(datetime.datetime.now())
+
+@app.route('/soma')
 def template():
-    return render_template('home.html')
+    return render_template('soma.html')
+
+@app.route('/soma', methods=['POST'])
+def my_form_post():
+    n1 = request.form['numero1']
+    n2 = request.form['numero2']
+
+    return f"a soma dos dois numero deu {float(n1) + float(n2)}"
